3a27e357cabd77a932c442da3f693933df550721
DefinitelyUnlikely <martin@kaarjohansson.se>
2024-11-29 08:21:37 +0100

    Fix FitlerView Routing

--- Finance/AppShell.xaml.cs
+++ Finance/AppShell.xaml.cs
@@ -11,9 +11,10 @@ namespace Finance
             // Route for account creation
             Routing.RegisterRoute(nameof(CreateAccView), typeof(CreateAccView));
 
-            // Routes for transactions and sorting
+            // Routes for transactions, sorting and filtering
             Routing.RegisterRoute(nameof(TransactionView), typeof(TransactionView));
             Routing.RegisterRoute(nameof(SortView), typeof(SortView));
+            Routing.RegisterRoute(nameof(FilterView), typeof(FilterView));
 
             // Routes for income and expense
             Routing.RegisterRoute(nameof(IncomeView), typeof(IncomeView));
