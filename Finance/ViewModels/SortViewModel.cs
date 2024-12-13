af7e41ee7a93c6d6ad4590c4e1ab7d5b6c859955
DefinitelyUnlikely <martin@kaarjohansson.se>
2024-12-13 14:28:12 +0100

    Add comment about error

--- Finance/ViewModels/SortViewModel.cs
+++ Finance/ViewModels/SortViewModel.cs
@@ -21,12 +21,18 @@ public partial class SortViewModel : ObservableObject
     // an observableCollection. 
     List<DisplayItem> mediatorList = [];
 
-
+    // For some reason, 'Transactions' is empty when the constructor runs. Both the copy and list in the VM.
+    // if you've gotten here from one of the accountViews. I would prefer not having to do
+    // another database query to get the transactions yet again.
     public SortViewModel(TransactionViewModel transactionViewModel)
     {
         this.transactionViewModel = transactionViewModel;
         Transactions = new List<Transaction>(transactionViewModel.Transactions);
 
+        foreach (Transaction transaction in transactionViewModel.Transactions)
+        {
+            Console.WriteLine(transaction.Name);
+        }
         dictionaries = DateKey.CreateTransactionDicts(Transactions);
 
         Year();
